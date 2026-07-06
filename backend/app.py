89ed3246e9eea6e644834296b393fc9b81117705
Rickey Johnston <rickey@Rickeys-MacBook-Pro-2.local>
2026-07-06 17:46:35 -0400

    Add initial Flask API

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "MLB Game Predictor API is running"
+    })
+
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "ok"
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
